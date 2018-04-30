73eba3fd6e794d80e6b658cea794bfd709752769
Lora Rose <aroseartist@gmail.com>
2018-04-29 17:27:49 -0700

    Simplification of code

--- Ch9-Playground.py
+++ Ch9-Playground.py
@@ -3,32 +3,31 @@
 # dictionary. It doesn't matter what the values are. Then you can use the in
 # operator as a fast way to check whether a string is in the dictionary.
 
-# text_words_dict = dict()
-# read_words = open('words.txt', 'r')
-# for each_line in read_words:
-#     clean_line = each_line.rstrip().split()
-#     for word in clean_line:
-#         lower_word = word.lower()
-#         if lower_word not in text_words_dict:
-#             text_words_dict[lower_word] = 1
-#         else:
-#             # word
-#             text_words_dict[lower_word] = text_words_dict[lower_word] + 1
-# print(text_words_dict)
-# read_words.close()
+text_words_dict = dict()
+read_words = open('words.txt', 'r')
+for each_line in read_words:
+    clean_line = each_line.split()
+    for word in clean_line:
+        lower_word = word.lower()
+        if lower_word not in text_words_dict:
+            text_words_dict[lower_word] = 1
+        else:
+            text_words_dict[lower_word] = text_words_dict[lower_word] + 1
+print('\nWords in dict:', text_words_dict)
+read_words.close()
 
 
 # ################# DICTIONARY BASICS 1.2 ################# #
 
-# text_words_dict = dict()
-# read_words = open('words.txt', 'r')
-# for each_line in read_words:
-#     clean_line = each_line.rstrip().split()
-#     for word in clean_line:
-#         lower_word = word.lower()
-#         text_words_dict[lower_word] = text_words_dict.get(lower_word, 0) + 1
-# print(text_words_dict)
-# read_words.close()
+text_words_dict = dict()
+read_words = open('words.txt', 'r')
+for each_line in read_words:
+    clean_line = each_line.split()
+    for word in clean_line:
+        lower_word = word.lower()
+        text_words_dict[lower_word] = text_words_dict.get(lower_word, 0) + 1
+print('\nWords in dict 2:', text_words_dict)
+read_words.close()
 
 
 # ################# DICTIONARY BASICS 2 ################# #
@@ -38,18 +37,19 @@
 # end of the program print out the contents of your dictionary (order does not
 # matter). Sample Line: From stephen.marquard@uct.ac.za Sat Jan  5 09:14:16 2008
 
-# def day_from():
-#     days_dict = dict()
-#     read_words = open('mbox-short.txt', 'r')
-#     for line in read_words:
-#         if line.rstrip().startswith('From '):
-#             split_line = line.split()
-#             day = split_line[2]
-#             days_dict[day] = days_dict.get(day, 0) + 1
-#     print(days_dict)
-#
-#
-# day_from()
+def day_from():
+    days_dict = dict()
+    read_words = open('mbox-short.txt', 'r')
+    for line in read_words:
+        if line.startswith('From '):
+            split_line = line.split()
+            day = split_line[2]
+            days_dict[day] = days_dict.get(day, 0) + 1
+    print('\nWeekday:', days_dict)
+    read_words.close()
+
+
+day_from()
 
 
 # ################# DICTIONARY BASICS 3 ################# #
@@ -57,18 +57,19 @@
 # to count how many messages have come from each email address, and print the
 # dictionary.
 
-# def emails():
-# from_whom = dict()
-#     read_lines = open('mbox-short.txt', 'r')
-#     for line in read_lines:
-#         if line.rstrip().startswith('From'):
-#             words = line.split()
-#             email = words[1]
-#             from_whom[email] = from_whom.get(email, 1) + 1
-#     print(from_whom)
-#
-#
-# emails()
+def emails():
+    from_whom = dict()
+    read_lines = open('mbox-short.txt', 'r')
+    for line in read_lines:
+        if line.startswith('From'):
+            words = line.split()
+            email = words[1]
+            from_whom[email] = from_whom.get(email, 0) + 1
+    print('\nFrom whom:', from_whom)
+    read_lines.close()
+
+
+emails()
 
 
 # ################# DICTIONARY BASICS 4 ################# #
@@ -77,22 +78,23 @@
 # through the dictionary using a maximum loop (see Section [maximumloop]) to find
 # who has the most messages and print how many messages the person has.
 
-# def emails():
-#     from_whom = dict()
-#     read_lines = open('mbox-short.txt', 'r')
-#     for line in read_lines:
-#         if line.rstrip().startswith('From'):
-#             # print(line)
-#             words = line.split()
-#             email = words[1]
-#             # print(words)
-#             from_whom[email] = from_whom.get(email, 1) + 1
-#             max_value = max(from_whom.values())
-#     print(from_whom)
-#     print('Most emailed:', email, max_value)
-#
-#
-# emails()
+def emails():
+    from_whom = dict()
+    read_lines = open('mbox-short.txt', 'r')
+    for line in read_lines:
+        if line.startswith('From '):
+            words = line.split()
+            email = words[1]
+            from_whom[email] = from_whom.get(email, 0) + 1
+            max_value = max(from_whom, key=lambda x: from_whom.get(x))
+            min_value = min(from_whom, key=lambda x: from_whom.get(x))
+    print(from_whom.values())
+    print('\nEmailed most:', max_value)
+    print('Emailed least:', min_value)
+    read_lines.close()
+
+
+emails()
 
 
 # ################# DICTIONARY BASICS 5 ################# #
@@ -109,7 +111,8 @@ def domain(given_file):
             find_email = words[1]
             from_domain = find_email.split('@')
             domain_dict[from_domain[1]] = domain_dict.get(from_domain[1], 1) + 1
-    print(domain_dict)
+    print('\nWhich domain:', domain_dict)
+    open_file.close()
 
 
 domain('mbox-short.txt')
